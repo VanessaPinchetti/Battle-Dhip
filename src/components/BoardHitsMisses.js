57f47d0a551d4d5643a8590141b044565c585c83
Nicolás Aravena <90357134+TheNoa96@users.noreply.github.com>
2022-05-12 11:53:14 -0400

    Update BoardHitsMisses.js

--- src/components/BoardHitsMisses.js
+++ src/components/BoardHitsMisses.js
@@ -2,6 +2,7 @@ import StyledHit from "../styled-components/styled-hit";
 import StyledMiss from "../styled-components/styled-miss";
 
 export default function BoardHitsMisses({ hits, misses }) {
+  // Muestra si acierta o falla el hit
   const getKey = (pos) => `(${pos.x}, ${pos.y})`;
   return (
     <>
